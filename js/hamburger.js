5ee80fc90bcd568a93758016de3084507400907d
Ahzam Syed <ahzam183@gmail.com>
2022-02-22 17:14:36 +0530

    fifth commit

--- js/hamburger.js
+++ js/hamburger.js
@@ -4,8 +4,18 @@ let navList = document.querySelector('.nav-list')
 let icon = document.querySelector('.icons')
 
 
-ham.addEventListener('click',function(){
-    navList.classList.toggle('v-class')
+// ham.addEventListener('click',function(){
+//     navList.classList.toggle('v-class')
+//     icon.classList.toggle('v-class')
+//     topBox.classList.toggle('h-nav')
+// })
+
+
+ham.addEventListener('click', function() {
+    if (window.innerWidth <= 880) {
+
+        navList.classList.toggle('togle')
+    }
+
     icon.classList.toggle('v-class')
-    topBox.classList.toggle('h-nav')
 })
