d868b0d389df03764c1a3b78c1aec7053d065740
Ahzam Syed <ahzam183@gmail.com>
2022-02-26 06:35:36 +0530

    updated

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,19 +1,22 @@
 let burger = document.querySelector('.hamburger')
-let ham = document.querySelector('.ham')
 let cross = document.querySelector('.close')
 let navList = document.querySelector('.nav-list')
 
-cross.style.display = 'none'
-burger.addEventListener('click', function () {
+// cross.style.display = 'none'
+// burger.addEventListener('click', function () {
+//     navList.classList.toggle('navGo')
+//     if (navList.classList.contains('navGo')) {
+//         ham.style.display = 'inline'
+//         cross.style.display = 'none'
+//     }
+//     else {
+//         ham.style.display = 'none'
+//         setTimeout(() => {
+//             cross.style.display = 'inline';
+//         }, 350);
+//     }
+// })
+
+function temptre(){
     navList.classList.toggle('navGo')
-    if (navList.classList.contains('navGo')) {
-        ham.style.display = 'inline'
-        cross.style.display = 'none'
-    }
-    else {
-        ham.style.display = 'none'
-        setTimeout(() => {
-            cross.style.display = 'inline';
-        }, 350);
-    }
-})
+}
